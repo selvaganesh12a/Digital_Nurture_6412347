cf449e57a3ac3dd9e95a879e868ed3a60aa15824
SELVA GANESH V <135496583+selvaganesh12a@users.noreply.github.com>
2025-06-27 15:16:44 +0530

    Mockito Exercise 1, 2 Completed
    Successfully Completed the Mockito Exercise 1, 2

---
+++ Hands_On_Practice/Week 2/Mockito/Exercise_1/MockingAndStubbing/src/test/java/com/example/MyServiceTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MyServiceTest {
+    @Test
+    public void testExternalApi(){
+        ExternalApi mockApi = mock(ExternalApi.class);
+        when(mockApi.getData()).thenReturn("Mock Data");
+        MyService service = new MyService(mockApi);
+        String result = service.fetchData();
+        assertEquals("Mock Data", result);
+    }
+}
