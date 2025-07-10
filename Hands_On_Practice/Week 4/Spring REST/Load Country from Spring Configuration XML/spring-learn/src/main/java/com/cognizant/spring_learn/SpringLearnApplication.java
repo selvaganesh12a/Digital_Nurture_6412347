b70e27c397de7ad211022ca3514f531d80210064
selvaganesh12a <selvaganesh12a@gmail.com>
2025-07-10 14:51:21 +0530

    Spring REST Exercises

---
+++ Hands_On_Practice/Week 4/Spring REST/Load Country from Spring Configuration XML/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,27 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+	public static void main(String[] args) {
+	    LOGGER.info("Start");
+		displayCountry();
+		LOGGER.info("End");
+	}
+
+	public static void displayCountry() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+		Country country = context.getBean("country", Country.class);
+		LOGGER.debug("Country : {}", country.toString());
+	}
+
+}
