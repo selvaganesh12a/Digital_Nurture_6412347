f35dd52d15cb327439225be8b4056e0d5d2a33b6
selvaganesh12a <selvaganesh12a@gmail.com>
2025-07-26 15:48:13 +0530

    Hands-On TrainersApp

---
+++ Hands_On_Practice/Week 6/TrainersApp/trainersapp/src/Home.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Welcome To My Academy Trainers Page</h2>
+        </div>
+    )
+}
+
+export default Home;
